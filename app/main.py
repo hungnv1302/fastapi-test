f8262c621e868ebc91506a189bafa7ce7aa3e31f
hungnv1302 <hunn1302@gmail.com>
2026-03-12 22:09:01 +0700

    add requirements

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from app.oauth2 import SECRET_KEY
 from . import models
 from .database import engine
@@ -9,6 +9,17 @@ from .config import settings
 # models.Base.metadata.create_all(bind = engine)
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(post.router)
 app.include_router(user.router)
